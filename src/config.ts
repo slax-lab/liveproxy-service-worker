f16ced75aac935ed05bb9ea2f24e4dabb83ed678
daguang <daguang830@gmail.com>
2025-04-18 12:39:12 +0800

    🚑 fix regex text match esm

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,12 @@ export const disablePostHost = [/mp\.weixin\.qq\.com/];
 export const proxyPrefix = "https://wabac-test.slax.dev/proxy/";
 
 export const REPLAY_URL_PREFIX =
-  /(?:\/proxy\/(?:([0-9]*)([a-z]{2})_)?\/|\/w\/liveproxy\/(?:([a-z]{2})_)?\/)(https?:\/\/.+)/;
+  /(?:\/proxy\/(?:([0-9]*)([a-z]{2,3})_)?\/|\/w\/liveproxy\/(?:([a-z]{2,3})_)?\/)(https?:\/\/.+)/;
+
+export const globalOverrides = [
+  "window",
+  "globalThis",
+  "self",
+  "document",
+  "location",
+];
